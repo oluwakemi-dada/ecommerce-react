8627f4cc22e56aed8626b6101c435e46ab79e9c9
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2025-06-25 21:23:20 +0100

    chore: first commit

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off', // Not needed with Vite/React 17+
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
