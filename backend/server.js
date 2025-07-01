ddb685107b0e1259dad16d233bfd88fe2b45dd9b
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2025-07-01 23:35:47 +0100

    chore: connect server to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 dotenv.config();
+
+connectDB(); // Connect to MongoDB
 
 const app = express();
 const port = process.env.PORT || 5001;
